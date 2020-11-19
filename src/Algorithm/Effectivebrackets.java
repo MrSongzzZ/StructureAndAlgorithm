ffc72626213205dc17a12547ad47478d4163172c
648828674@qq.com <jiashuai>
2020-11-19 17:14:55 +0800

    BFS

---
+++ src/Algorithm/Effectivebrackets.java
@@ -0,0 +1,51 @@
+package Algorithm;
+
+import java.util.Stack;
+
+/**
+ *  给定⼀个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，
+ *  判断字符串 是否有效。
+ *  有效字符串需满⾜：
+ *  1. 左括号必须⽤相同类型的右括号闭合。
+ *  2. 左括号必须以正确的顺序闭合。
+ *  * 注意空字符串可被认为是有效字符串。
+ *  示例 1： 输⼊: "()"
+ *  输出: true
+ *  示例 2： 输⼊: "(]"
+ *  输出: false
+ *
+ */
+public class Effectivebrackets {
+
+    private static boolean check(String str) {
+        Stack<Character> stack = new Stack<>();
+        if (("").equals(str)) {
+            return true;
+        }
+
+        if (str.length() % 2 != 0) {
+            return false;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            char a = str.charAt(i);
+            if (a == '(' || a == '{' || a == '[') {
+                stack.push(a);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                } else {
+                    char b = stack.pop();
+                    if ((a == ')' && b != '(') || (a == ']' && b != '[') || (a == '}' && b != '{')) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
