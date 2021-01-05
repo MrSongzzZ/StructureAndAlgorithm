1abbd1392c3ef68beebf8adaacbc98c83907f4c0
648828674@qq.com <jiashuai>
2021-01-05 11:16:49 +0800

    最开始想到的就是遍历 其实就是中序遍历 中序遍历就是线性的结果
    优化之后 反向中序 + 提前返回

---
+++ src/Algorithm/kthLargest.java
@@ -0,0 +1,85 @@
+package Algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给定一棵二叉搜索树，请找出其中第k大的节点。
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ * 输入: root = [3,1,4,null,2], k = 1
+ *    3
+ *   / \
+ *  1   4
+ *   \
+ *    2
+ * 输出: 4
+ * 示例 2:
+ *
+ * 输入: root = [5,3,6,2,4,null,null,1], k = 3
+ *        5
+ *       / \
+ *      3   6
+ *     / \
+ *    2   4
+ *   /
+ *  1
+ * 输出: 4
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/er-cha-sou-suo-shu-de-di-kda-jie-dian-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class kthLargest {
+    //优化一下  反向中序 + 提前返回
+//    List<Integer> integers = new ArrayList<>();
+    int result = 0;
+    int ak = 0;
+    public int kthLargest(TreeNode root, int k) {
+        if (root == null) {
+            return 0;
+        }
+        ak = k;
+        if (root.right != null) {
+            forT(root.right);
+        }
+//        integers.add(root.val);
+        ak--;
+        if (ak == 0) {
+            result = root.val;
+            return result;
+        }
+        if (root.left != null) {
+            forT(root.left);
+        }
+        return result;
+    }
+
+    public void forT(TreeNode s) {
+        if (ak == 0) {
+            return;
+        }
+        if (s.right != null) {
+            forT(s.right);
+        }
+//        integers.add(s.val);
+        ak--;
+        if (ak == 0) {
+            result = s.val;
+            return;
+        }
+        if (s.left != null) {
+            forT(s.left);
+        }
+    }
+
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+    }
+}
