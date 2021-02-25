7477ca73480a818ad28ac6ef2420574950166b59
648828674@qq.com <jiashuai>
2021-02-25 15:51:21 +0800

    即时编译器

---
+++ src/Algorithm/transpose.java
@@ -0,0 +1,47 @@
+package Algorithm;
+
+/**
+ * 给你一个二维整数数组 matrix， 返回 matrix 的 转置矩阵 。
+ *
+ * 矩阵的 转置 是指将矩阵的主对角线翻转，交换矩阵的行索引与列索引。
+ *
+ *
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：matrix = [[1,2,3],[4,5,6],[7,8,9]]
+ * 输出：[[1,4,7],[2,5,8],[3,6,9]]
+ * 示例 2：
+ *
+ * 输入：matrix = [[1,2,3],[4,5,6]]
+ * 输出：[[1,4],[2,5],[3,6]]
+ *  
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/transpose-matrix
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class transpose {
+
+    public static int[][] transpose(int[][] matrix) {
+        if (matrix.length == 0) {
+            return new int[0][];
+        }
+        int[][] result = new int[matrix[0].length][matrix.length];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                result[j][i] = matrix[i][j];
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1,2,3}, {4,5,6},{7,8,9}};
+        transpose(matrix);
+    }
+}
