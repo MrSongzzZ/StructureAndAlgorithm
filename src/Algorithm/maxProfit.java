c1719f7b5b9012509ab21afa3497e662e17be84f
“me <648828674@qq.com>
2021-02-27 12:52:43 +0800

    加油

--- src/Algorithm/maxProfit.java
+++ src/Algorithm/maxProfit.java
@@ -33,6 +33,15 @@ package Algorithm;
  */
 public class maxProfit {
     public int maxProfit(int[] prices) {
-
+        int min = Integer.MAX_VALUE;
+        int result = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < min) {
+                min = prices[i];
+            } else {
+                result = Math.max(result, prices[i] - min);
+            }
+        }
+        return result;
     }
 }
