aa689f5a0ffeb2aeb46be2bd4e4d9323b7d8557f
“me <648828674@qq.com>
2021-02-27 20:08:54 +0800

    加油

---
+++ src/Algorithm/merge.java
@@ -0,0 +1,54 @@
+package Algorithm;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * 以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。请你合并所有重叠的区间，并返回一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：intervals = [[1,3],[2,6],[8,10],[15,18]]
+ * 输出：[[1,6],[8,10],[15,18]]
+ * 解释：区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+ * 示例 2：
+ *
+ * 输入：intervals = [[1,4],[4,5]]
+ * 输出：[[1,5]]
+ * 解释：区间 [1,4] 和 [4,5] 可被视为重叠区间。
+ *  
+ */
+public class merge {
+
+    public static int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            public int compare(int[] interval1, int[] interval2) {
+                return interval1[0] - interval2[0];
+            }
+        });
+        List<int[]> result = new ArrayList<>();
+        int[] temp = intervals[0];
+        result.add(temp);
+        for (int i = 1; i < intervals.length; i++) {
+            int[] now = intervals[i];
+            if (now[0] <= temp[1]) {
+                if (now[1] > temp[1]) {
+                    temp[1] = now[1];
+                }
+            } else {
+                temp = now;
+                result.add(temp);
+            }
+        }
+        return result.toArray(new int[result.size()][]);
+    }
+
+    public static void main(String[] args) {
+        int[][] intervals = {{1,3},{2,6},{8,10},{15,18}};
+        System.out.println(merge(intervals));
+    }
+}
