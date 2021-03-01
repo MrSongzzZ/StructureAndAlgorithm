1efedf23fd0df95be4c5c9c6a66ff0a83e07af17
648828674@qq.com <jiashuai>
2021-03-01 14:48:10 +0800

    逐步分析 解决问题

---
+++ src/Algorithm/minIncrementForUnique.java
@@ -0,0 +1,48 @@
+package Algorithm;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+import java.util.Arrays;
+
+/**
+ * q945
+ * 给定整数数组 A，每次 move 操作将会选择任意 A[i]，并将其递增 1。
+ *
+ * 返回使 A 中的每个值都是唯一的最少操作次数。
+ *
+ * 示例 1:
+ *
+ * 输入：[1,2,2]
+ * 输出：1
+ * 解释：经过一次 move 操作，数组将变为 [1, 2, 3]。
+ * 示例 2:
+ *
+ * 输入：[3,2,1,2,1,7]
+ * 输出：6
+ * 解释：经过 6 次 move 操作，数组将变为 [3, 4, 1, 2, 5, 7]。
+ * 可以看出 5 次或 5 次以下的 move 操作是不能让数组的每个值唯一的。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/minimum-increment-to-make-array-unique
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class minIncrementForUnique {
+
+    public static int minIncrementForUnique(int[] A) {
+        int result = 0;
+        Arrays.sort(A);
+        for (int i = 1; i < A.length; i++) {
+            if (A[i] <= A[i - 1]) {
+                int temp = A[i - 1] + 1;
+                result += temp - A[i];
+                A[i] = temp;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(minIncrementForUnique(new int[]{1,2,2}));
+        System.out.println(minIncrementForUnique(new int[]{3,2,1,2,1,7}));
+    }
+}
