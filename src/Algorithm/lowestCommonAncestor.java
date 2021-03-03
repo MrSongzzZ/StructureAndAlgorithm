f8a2383f579f1ec75319892f2a3a8110096e3c88
648828674@qq.com <jiashuai>
2021-03-03 18:42:09 +0800

    逐步分析 解决问题

--- src/Algorithm/lowestCommonAncestor.java
+++ src/Algorithm/lowestCommonAncestor.java
@@ -18,58 +18,74 @@ import java.util.Map;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class lowestCommonAncestor {
-    int pLength = 0;
-    int qLength = 0;
-    int qflag = 0;
-    int pflag = 0;
-    Map<TreeNode, TreeNode> map = new HashMap<>();
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null) {
+        if (root == null || root == p || root == q) {
             return root;
         }
-        tree(root, p, q, 1);
-        while (qLength < pLength) {
-            pLength--;
-            p = map.get(p);
-        }
 
-        while (pLength < qLength) {
-            qLength--;
-            q = map.get(q);
-        }
-
-        while (pLength >= 0) {
-            if (q == p) {
-                return q;
-            } else {
-                q = map.get(q);
-                p = map.get(p);
-            }
-        }
-        return root;
-    }
-
-    public void tree(TreeNode root, TreeNode p, TreeNode q, int tempLength) {
-        if (root.left != null) {
-            map.put(root.left, root);
-        }
-        if (root.right != null) {
-            map.put(root.right, root);
-        }
-        if (root == p) {
-            pflag = 1;
-            pLength = tempLength;
-        }
-        if (root == q) {
-            qflag = 1;
-            qLength = tempLength;
-        }
-        if (qflag + pflag != 2 && root.left != null) {
-            tree(root.left, p, q, tempLength + 1);
-        }
-        if (qflag + pflag != 2 && root.right != null) {
-            tree(root.right, p, q, tempLength + 1);
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        } else if (left == null) {
+            return right;
+        } else {
+            return left;
         }
     }
+//    int pLength = 0;
+//    int qLength = 0;
+//    int qflag = 0;
+//    int pflag = 0;
+//    Map<TreeNode, TreeNode> map = new HashMap<>();
+//
+//    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//        if (root == null) {
+//            return root;
+//        }
+//        tree(root, p, q, 1);
+//        while (qLength < pLength) {
+//            pLength--;
+//            p = map.get(p);
+//        }
+//
+//        while (pLength < qLength) {
+//            qLength--;
+//            q = map.get(q);
+//        }
+//
+//        while (pLength >= 0) {
+//            if (q == p) {
+//                return q;
+//            } else {
+//                q = map.get(q);
+//                p = map.get(p);
+//            }
+//        }
+//        return root;
+//    }
+//
+//    public void tree(TreeNode root, TreeNode p, TreeNode q, int tempLength) {
+//        if (root.left != null) {
+//            map.put(root.left, root);
+//        }
+//        if (root.right != null) {
+//            map.put(root.right, root);
+//        }
+//        if (root == p) {
+//            pflag = 1;
+//            pLength = tempLength;
+//        }
+//        if (root == q) {
+//            qflag = 1;
+//            qLength = tempLength;
+//        }
+//        if (qflag + pflag != 2 && root.left != null) {
+//            tree(root.left, p, q, tempLength + 1);
+//        }
+//        if (qflag + pflag != 2 && root.right != null) {
+//            tree(root.right, p, q, tempLength + 1);
+//        }
+//    }
 }
