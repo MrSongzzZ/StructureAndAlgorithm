37361ea24d5ad9e8761118ece42dc9a8598ca120
648828674@qq.com <jiashuai>
2021-03-05 11:17:57 +0800

    逐步分析 解决问题

--- src/Algorithm/maxSubArray.java
+++ src/Algorithm/maxSubArray.java
@@ -36,10 +36,10 @@ public class maxSubArray {
         }
         return max;
     }
-
+//
     public static void main(String[] args) {
-//        int[] nums = new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4};
-        int[] nums = new int[]{1, 2};
+        int[] nums = new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4};
+//        int[] nums = new int[]{1, 2};
         System.out.println(maxSubArray(nums));
     }
 }
