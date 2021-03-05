fbe3684790286b315600f47ccc38a14c8a5ae58a
648828674@qq.com <jiashuai>
2021-03-05 14:51:35 +0800

    逐步分析 解决问题

---
+++ src/Algorithm/generate.java
@@ -0,0 +1,50 @@
+package Algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给定一个非负整数 numRows，生成杨辉三角的前 numRows 行。
+ *
+ *
+ *
+ * 在杨辉三角中，每个数是它左上方和右上方的数的和。
+ *
+ * 示例:
+ *
+ * 输入: 5
+ * 输出:
+ * [
+ *      [1],
+ *     [1,1],
+ *    [1,2,1],
+ *   [1,3,3,1],
+ *  [1,4,6,4,1]
+ * ]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/pascals-triangle
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class generate {
+
+    public static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> temp = new ArrayList<>(i + 1);
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    temp.add(1);
+                } else {
+                    temp.add(result.get(i - 1).get(j) + result.get(i - 1).get(j - 1));
+                }
+            }
+            result.add(temp);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        generate(5);
+    }
+}
