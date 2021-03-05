7b965b5e53b4a67d98b47da2c726cf608cdbc829
648828674@qq.com <jiashuai>
2021-03-05 11:19:17 +0800

    逐步分析 解决问题

--- src/Algorithm/maxSubArray.java
+++ src/Algorithm/maxSubArray.java
@@ -31,7 +31,8 @@ public class maxSubArray {
     public static int maxSubArray(int[] nums) {
         int max = nums[0];
         for (int i = 1; i < nums.length; i++) {
-            nums[i] += Math.max(nums[i - 1], 0);
+//            nums[i] += Math.max(nums[i - 1], 0);
+            nums[i] = Math.max(nums[i - 1] + nums[i], nums[i]);
             max = Math.max(nums[i], max);
         }
         return max;
