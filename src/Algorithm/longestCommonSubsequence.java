991b741c9efdff4a094af67b12194fe5fb244faf
“me <648828674@qq.com>
2021-03-06 11:51:48 +0800

    回溯算法

--- src/Algorithm/longestCommonSubsequence.java
+++ src/Algorithm/longestCommonSubsequence.java
@@ -38,12 +38,22 @@ public class longestCommonSubsequence {
     public int longestCommonSubsequence(String text1, String text2) {
         int n = text1.length();
         int m = text2.length();
-        int[][] dp = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-
+        int[][] dp = new int[n + 1][m + 1];
+        int result = 0;
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                } else {
+                    if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                        dp[i][j] = dp[i - 1][j - 1] + 1;
+                    } else {
+                        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    }
+                }
+                result = Math.max(result, dp[i][j]);
             }
         }
-        return 0;
+        return result;
     }
 }
