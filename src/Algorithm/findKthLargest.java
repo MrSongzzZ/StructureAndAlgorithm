2af5c4369288d6cf0690d11ac2b3165b03e99854
“me <648828674@qq.com>
2021-03-07 17:04:37 +0800

    回溯算法

--- src/Algorithm/findKthLargest.java
+++ src/Algorithm/findKthLargest.java
@@ -1,23 +1,63 @@
 package Algorithm;
 
-import java.util.Stack;
 
+import java.awt.event.ItemEvent;
+
+/**
+ * q215
+ * 在未排序的数组中找到第 k 个最大的元素。请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
+ *
+ * 示例 1:
+ *
+ * 输入: [3,2,1,5,6,4] 和 k = 2
+ * 输出: 5
+ * 示例 2:
+ *
+ * 输入: [3,2,3,1,2,4,5,5,6] 和 k = 4
+ * 输出: 4
+ *
+ */
 public class findKthLargest {
 
-    public int findKthLargest(int[] nums, int k) {
-        Stack<Integer> stack =new Stack<>();
-        int larger = 0;
-        int smaller = 0;
-        int result = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (!stack.isEmpty()) {
-                if (stack.peek() >= nums[i]) {
-                    smaller ++;
-                }
-            } else {
-                stack.push(nums[i]);
-            }
+    public static int findKthLargest(int[] nums, int k) {
+        int heapSize = nums.length;
+        buildHead(nums, heapSize);
+        for (int i = heapSize; i >= k; i--) {
+            nums[0] = nums[heapSize - 1];
+            heapSize--;
+            minHeap(nums, 0, heapSize);
         }
-        return result;
+        return nums[0];
+    }
+
+    private static void buildHead(int[] nums, int heapSize) {
+        for (int i = (heapSize - 1) / 2; i >= 0; i--) {
+            minHeap(nums, i, heapSize);
+        }
+    }
+
+    public static void minHeap(int[] temp, int i, int heapSize) {
+        int left = i * 2 + 1;
+        int right = i * 2 + 2;
+        int smallest = -1;
+        if (left < heapSize && temp[i] > temp[left]) {
+            smallest = left;
+        } else {
+            smallest = i;
+        }
+        if (right < heapSize && temp[smallest] > temp[right]) {
+            smallest = right;
+        }
+        if (smallest != i) {
+            int tempNum = temp[smallest];
+            temp[smallest] = temp[i];
+            temp[i] = tempNum;
+            minHeap(temp, smallest, heapSize);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findKthLargest(new int[]{3,2,1,5,6,4}, 2));
+//        System.out.println(findKthLargest(new int[]{-1, -1}, 2));
     }
 }
