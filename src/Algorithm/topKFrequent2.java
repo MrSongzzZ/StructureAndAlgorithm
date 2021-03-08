a2f0b9e3feae08070ce307859103b9ebb09606b9
“me <648828674@qq.com>
2021-03-08 23:45:26 +0800

    回溯算法

---
+++ src/Algorithm/topKFrequent2.java
@@ -0,0 +1,72 @@
+package Algorithm;
+
+import java.util.*;
+
+/**
+ * q692_前K个高频单词
+ *
+ 给一非空的单词列表，返回前 k 个出现次数最多的单词。
+ 返回的答案应该按单词出现频率由高到低排序。如果不同的单词有相同出现频率，按字母顺序排序。
+ 示例 1：
+ 输入: ["i", "love", "leetcode", "i", "love", "coding"], k = 2
+ 输出: ["i", "love"]
+ 解析: "i" 和 "love" 为出现次数最多的两个单词，均为2次。
+ 注意，按字母顺序 "i" 在 "love" 之前。
+ 示例 2：
+ 输入: ["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"], k = 4
+ 输出: ["the", "is", "sunny", "day"]
+ 解析: "the", "is", "sunny" 和 "day" 是出现次数最多的四个单词，
+ 出现次数依次为 4, 3, 2 和 1 次。
+ 注意：
+ 假定 k 总为有效值， 1 ≤ k ≤ 集合元素数。
+ 输入的单词均由小写字母组成。
+ *
+ *
+ */
+public class topKFrequent2 {
+    //桶排序
+    public List<String> topKFrequent1(String[] words, int k) {
+        int max = 0;
+        Map<String, Integer> map = new HashMap<>();
+        for (String word : words) {
+            int num = map.getOrDefault(word, 0) + 1;
+            max = Math.max(num, max);
+            map.put(word, num);
+        }
+
+        List<String>[] list = new List[max + 1];
+        for (String s : map.keySet()) {
+            int num = map.get(s);
+            if (list[num] == null) {
+                list[num] = new ArrayList<>();
+            }
+            list[num].add(s);
+        }
+        List<String> result = new ArrayList<>();
+        for (int i = list.length - 1; i >= 0; i--) {
+            if (list[i] != null) {
+                Collections.sort(list[i], new Comparator<String>() {
+                    @Override
+                    public int compare(String o1, String o2) {
+                        int a = map.get(o1);
+                        int b = map.get(o2);
+                        if (a == b) {
+                            return o1.compareTo(o2);
+                        } else {
+                            return b - a;
+                        }
+                    }
+                });
+                for (String s : list[i]) {
+                    result.add(s);
+                    if (k == result.size()) {
+                        return result;
+                    }
+                }
+
+            }
+        }
+        return result;
+    }
+
+}
